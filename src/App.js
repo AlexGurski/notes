c03e5265194eb12453bef41695312af543d07587
etosolo322 <etosolo322@yandex.ru>
2022-06-01 12:55:14 +0300

    starts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [date,setDate] = useState('')
+  useEffect(()=>{
+    fetch('http://localhost:3001/users')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      setDate(data);
+    });
+  },[])
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Object.keys(date).map(e=><p>{date[e].name}</p>)}
     </div>
   );
 }
