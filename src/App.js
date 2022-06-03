36ad6754ece19dd7154953323844f19689d923a3
etosolo322 <etosolo322@yandex.ru>
2022-06-03 18:05:26 +0300

    add post

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import { useEffect, useState } from "react";
 import './assets/style/notes.css'
 import {NotesListItems} from './layouts/NotesListItems'
 import {NotesTextItem} from './layouts/NotesTextItem'
+import axios from "axios";
 
 function App() {
   const [notes,setNotes] = useState(false)
   const [fullNotes, setFullNotes] = useState(false)
+  const [updateServer, setUpdateServer]=useState(true)
+const update = (data, path) =>{
+  console.log(data)
+        axios.put('http://localhost:3001/notes/'+path, data )
+        .then(resp => {
+          setUpdateServer(!updateServer)
+          //console.log(resp.data);
+      }).catch(error => {
+          console.log(error);
+      });
+}
+
   useEffect(()=>{
     fetch('http://localhost:3001/notes')
     .then((response) => {
@@ -14,13 +27,13 @@ function App() {
     .then((data) => {
       setNotes(data.reverse());
     });
-  },[])
+  },[updateServer])
 
   return (
     <>    
     <div className="notes">
-        <NotesListItems notes={notes} onClickOnText={notesTo=>setFullNotes(notesTo)}/>
-        <NotesTextItem fullNotes={fullNotes}/>
+        <NotesListItems notes={notes} onClickOnText={notesTo=>setFullNotes(notesTo)} />
+        <NotesTextItem fullNotes={fullNotes} updates={(data, path)=>update(data,path)}/>
     </div>
     </>
   );
--- src/layouts/NotesTextItem.js
+++ src/layouts/NotesTextItem.js
@@ -2,9 +2,11 @@ import { useEffect, useRef, useState } from 'react'
 import {FaSlackHash} from 'react-icons/fa'
 import {AiOutlineEdit, AiOutlineCheckSquare, AiOutlineCloseSquare} from 'react-icons/ai'
 import {BiMessageSquareAdd} from 'react-icons/bi'
-export const NotesTextItem = ({fullNotes})=>{
+
+export const NotesTextItem = ({fullNotes, updates})=>{
   const divText = useRef("text")
   const divTitle = useRef("text")
+  const divInput = useRef("text")
   const [text,setText] = useState(false);
   const [tags, setTags] = useState(null);
   const [editHeader, setEditHeader] = useState(false)
@@ -25,7 +27,9 @@ return (
       <span className='notes_text_header'>
         <h2  contentEditable={editHeader} ref={divTitle}>{text.title}</h2>  
         <AiOutlineEdit onClick={()=>setEditHeader(true)} style={{display:!editHeader?'block':'none'}}/>  
-        <AiOutlineCheckSquare style={{display:editHeader?'block':'none'}} onClick={()=>{setEditHeader(false); setText({...text, title:divTitle.current.textContent})}} />  
+        <AiOutlineCheckSquare style={{display:editHeader?'block':'none'}} onClick={()=>{setEditHeader(false); 
+                                                                                        setText({...text, title:divTitle.current.textContent});
+                                                                                        updates({...text, title:divTitle.current.textContent}, text.id)}} />  
         <AiOutlineCloseSquare style={{display:editHeader?'block':'none'}} onClick={()=>{setEditHeader(false); divTitle.current.textContent=text.title}}/>  
       </span>
       <div  className='notes_text_div' 
@@ -42,7 +46,20 @@ return (
         {
           text? Object.values(text.tags).map(el=><li onClick={()=>setTags(el)}>{el}</li>):null
         }
-          <BiMessageSquareAdd  style={{fontSize:'25px'}}/>  
+         <span className='notes_text_addHashTag'>
+         <BiMessageSquareAdd  style={{fontSize:'25px'}} onClick={()=>divInput.current.classList.add("animate")}/> 
+         <BiMessageSquareAdd  style={{fontSize:'25px'}} onClick={()=>divInput.current.classList.remove("animate")}/> 
+         <input type="text" className='disanimate' ref={divInput} onKeyPress={e=>
+          {
+            if ((e.key == 'Enter' || e.key == ' ') && e.target.value!==''){            
+              setText({...text, tags:[...text.tags, e.target.value]})
+              divInput.current.classList.remove("animate");
+              updates({...text, tags:[...text.tags, e.target.value]},text.id )
+              e.target.value=''             
+            }
+          }
+        }></input>
+        </span>  
       </ul>
     </div>
     :null
