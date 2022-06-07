3fbb24fc8a61aa334a5f61a2d2a03f8ad344ae86
etosolo322 <etosolo322@yandex.ru>
2022-06-07 13:35:13 +0300

    fgh

--- src/modules/query.js
+++ src/modules/query.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const URL = 'http://localhost:5000/notes/'
+const URL = 'https://testnotestask.herokuapp.com:5000/notes/'
 export const get = () =>{
     return fetch(URL)
    }
